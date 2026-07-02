7e0bbe6191c62fc4baeab89017990d26275fc349
Raulnik Royce Soares <raulnikroyce12@gmail.com>
2026-07-02 15:45:13 -0300

    add console.error no login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -15,6 +15,21 @@ exports.login = async (req, res) => {
     }
 };
 
+exports.login = async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+        const usuario = await authService.autenticar(email, senha);
+        
+        if (!usuario) return res.status(401).json({ erro: 'Credenciais inválidas' });
+
+        const token = jwt.sign({ id: usuario.id }, 'MINHA_CHAVE_SECRETA_MUITO_SEGURA', { expiresIn: '1h' });
+        res.json({ mensagem: 'Login realizado!', token });
+    } catch (error) {
+        console.error("ERRO DETALHADO NO LOGIN:", error); // <-- ADICIONE ESTA LINHA
+        res.status(500).json({ erro: 'Erro no servidor' });
+    }
+};
+
 exports.registrar = async (req, res) => {
     try {
         await authService.registrar(req.body.email, req.body.senha);
