b3acd441daae05bbbef672fff0f22bc864993106
Raulnik Royce Soares <raulnikroyce12@gmail.com>
2026-08-20 13:02:07 -0300

    refactor(bandas): simplify service layer

--- src/services/banda.service.js
+++ src/services/banda.service.js
@@ -1,16 +1,7 @@
-// src/services/banda.service.js
 const bandaRepository = require('../repositories/banda.repository');
 
-exports.listarBandas = async () => {
-    return await bandaRepository.buscarTodas();
-};
+exports.listarBandas = () => bandaRepository.buscarTodas();
 
-// Aqui está a peça que faltava!
-exports.obterBandaPorId = async (id) => {
-    return await bandaRepository.buscarPorId(id);
-};
+exports.obterBandaPorId = (id) => bandaRepository.buscarPorId(id);
 
-exports.adicionarBanda = async (dadosBanda) => {
-    // A validação do Zod já aconteceu lá na rota, então os dados aqui já estão limpos e seguros
-    return await bandaRepository.salvar(dadosBanda);
-};
+exports.adicionarBanda = (dadosBanda) => bandaRepository.salvar(dadosBanda);
