f148175db8a734bdb8b3f2bcd7b2c13167f4dc1f
Raulnik Royce Soares <raulnikroyce12@gmail.com>
2026-08-20 13:04:49 -0300

    refactor(routes): remove redundant comments from band routes

--- src/routes/banda.routes.js
+++ src/routes/banda.routes.js
@@ -1,19 +1,12 @@
-// src/routes/banda.routes.js
 const express = require('express');
 const router = express.Router();
 const bandaController = require('../controllers/banda.controller');
 const { verificarToken, verificarPerfil } = require('../middlewares/auth.middleware');
 const { validarSchema } = require('../middlewares/validador');
+const { bandaSchema } = require('../schemas/banda.schema');
 
-// Importação do schema do Zod (garanta que o nome do arquivo bate com o que você criou)
-const { bandaSchema } = require('../schemas/banda.schema'); 
-
-// Leitura: Qualquer um com token logado pode ver as bandas
 router.get('/', verificarToken, bandaController.listarBandas);
 router.get('/:id', verificarToken, bandaController.obterBandaPorId);
-
-// Criação: Exige Token, exige validação do Zod, E exige ser 'admin'
 router.post('/', verificarToken, verificarPerfil(['admin']), validarSchema(bandaSchema), bandaController.adicionarBanda);
 
-// A FALTA DESTA LINHA ABAIXO É O QUE CAUSA O ERRO NO APP.JS:
 module.exports = router;
